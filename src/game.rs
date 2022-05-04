8fad8e3cfddea641545680fc8891c4c48a82671c
tnantoka <tnantoka@bornneet.com>
2022-05-04 12:47:21 +0900

    feat: create project

---
+++ src/game.rs
@@ -0,0 +1,9 @@
+pub struct Game {
+
+}
+
+impl Game {
+  pub fn new() -> Self {
+    Game {}
+  }
+}
---
+++ src/main.rs
@@ -0,0 +1,6 @@
+use shiritori::game::{Game};
+
+fn main() {
+  let game = Game::new();
+  println!("hello");
+}
